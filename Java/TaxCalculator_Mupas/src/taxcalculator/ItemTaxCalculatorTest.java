ab0476761a9e1547a2f20179d0cb0ee158570884
Arrow <delmontearrow2004@gmai;.com>
2025-07-15 17:27:44 +0800

    TaxCalculator Activity

---
+++ Java/TaxCalculator_Mupas/src/taxcalculator/ItemTaxCalculatorTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package taxcalculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+class ItemTaxCalculatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Java/TaxCalculator_Mupas/src/taxcalculator/Test.java
@@ -0,0 +1,10 @@
+package taxcalculator;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+        names.forEach(name -> System.out.println(name));
+    }
+}
