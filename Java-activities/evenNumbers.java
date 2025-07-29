bcfc986520f7a26f42a1e4b50e696db4d79464c0
Arrow <delmontearrow2004@gmai;.com>
2025-07-29 23:07:07 +0800

    Java activies I have saved

---
+++ Java-activities/evenNumbers.java
@@ -0,0 +1,14 @@
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+        int x=0;
+        int y=2;
+        System.out.println("Even numbers");
+        while(x<5)
+        {
+            System.out.println(y);
+            x+=1;
+            y+=2;
+        }
+    }
+}
